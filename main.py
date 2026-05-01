db179f2a2ada3cf2b5c09f6fc8dd84d89bad567a
Purichaya Fraczyk <sopeach.a@gmail.com>
2026-05-01 11:12:30 +0700

    feat: Add schedule status check

--- main.py
+++ main.py
@@ -1,39 +1,20 @@
 import boto3
+import schedule
 
 ec2_client = boto3.client('ec2', region_name='ap-southeast-1')
 ec2 = boto3.resource('ec2', region_name='ap-southeast-1')
 
-new_vpc = ec2.create_vpc(
-    CidrBlock="10.0.0.0/16"
-)
+def check_instance_status():
+    statuses = ec2_client.describe_instance_status(
+        IncludeAllInstances=True
+    )
+    for status in statuses['InstanceStatuses']:
+        int_status = status['InstanceStatus']['Status']
+        sys_status = status['InstanceStatuses']['Status']
+        state = status['InstanceStatuses']['State']
+        print(f"Instance {status['InstanceId']} is {state} with instance status {int_status} and system status {sys_status}")
+    print("#######################################\n")
+schedule.every(5).minutes.do(check_instance_status)
 
-new_vpc.wait_until_available()
-
-new_vpc.create_subnet(
-    CidrBlock="10.0.1.0/24"
-)
-
-new_vpc.create_subnet(
-    CidrBlock="10.0.2.0/24"
-)
-
-new_vpc.create_tags(
-    Tags=[
-        {
-            'Key': 'Name',
-            'Value': 'my-vpc'
-        },
-    ]
-)
-
-
-all_available_vpcs = ec2_client.describe_vpcs()
-vpcs = all_available_vpcs["Vpcs"]
-
-#print(vpcs)
-
-for vpc in vpcs:
-    print(vpc["VpcId"])
-    cidr_block_assoc_sets = vpc["CidrBlockAssociationSet"]
-    for assoc_set in cidr_block_assoc_sets:
-        print(assoc_set["CidrBlockState"])
+while True:
+    schedule.run_pending()
